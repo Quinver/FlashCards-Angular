b349ce08acf924a141e517fa18bcd54968843d91
quinver150 <quinver15@gmail.com>
2024-12-22 02:51:18 +0100

    add card service and implement CRUD operations for cards

---
+++ src/app/card.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+interface Card {
+  id: number;
+  frontText: string;
+  backText: string;
+  flipped: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+  private apiUrl = 'http://localhost:5041/api/cards'; // Your API endpoint
+
+  constructor(private http: HttpClient) { }
+
+  // Get all cards
+  getCards(): Observable<Card[]> {
+    return this.http.get<Card[]>(this.apiUrl);
+  }
+
+  // Get a specific card by id
+  getCard(id: number): Observable<Card> {
+    return this.http.get<Card>(`${this.apiUrl}/${id}`);
+  }
+
+  // Create a new card
+  createCard(card: Card): Observable<Card> {
+    return this.http.post<Card>(this.apiUrl, card, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    });
+  }
+
+  // Update an existing card
+  updateCard(id: number, card: Card): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, card, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    });
+  }
+
+  // Delete a card
+  deleteCard(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { CardService } from '../card.service';
 import { CommonModule } from '@angular/common';
 
 interface Card {
+  id: number;
   flipped: boolean;
   frontText: string;
   backText: string;
@@ -14,13 +16,18 @@ interface Card {
   styleUrls: ['./cards.component.css'],
 })
 export class CardsComponent {
-  cards: Card[] = [
-    { frontText: 'Front 1', backText: 'Back 1', flipped: false },
-    { frontText: 'Front 2', backText: 'Back 2', flipped: false },
-    { frontText: 'Front 3', backText: 'Back 3', flipped: false },
-  ];
+  cards: Card[] = [];
 
   currentCardIndex = 0;
+
+  constructor(private cardService: CardService) {}
+
+  ngOnInit() {
+    // Fetch cards from the API when the component is initialized
+    this.cardService.getCards().subscribe((data: Card[]) => {
+      this.cards = data;
+    });
+  }
 
   flipCard(index: number) {
     // Toggle the flipped state based on the card index and reset to front
@@ -37,5 +44,19 @@ export class CardsComponent {
     // Decrement the current card index and wrap around using modulo
     this.cards.forEach(card => card.flipped = false);
     this.currentCardIndex = (this.currentCardIndex - 1 + this.cards.length) % this.cards.length;
+  }
+  
+  addCard() {
+    const newCard: Card = { id: 0, frontText: 'New Front', backText: 'New Back', flipped: false };
+    this.cardService.createCard(newCard).subscribe((card: Card) => {
+      this.cards.push(card); // Add the new card to the list
+    });
+  }
+
+  // Delete a card
+  deleteCard(id: number) {
+    this.cardService.deleteCard(id).subscribe(() => {
+      this.cards = this.cards.filter(card => card.id !== id); // Remove the deleted card from the list
+    });
   }
 }
